2aa145fa1ae426de48e18491966e3e7b7509a697
erietz <ewrietz@gmail.com>
2022-12-06 19:20:29 -0600

    get rid of some variables

--- src/day06/day06.go
+++ src/day06/day06.go
@@ -14,17 +14,15 @@ func Solve(input string) {
 }
 
 func part1(stream string) (int, bool) {
-	s := stream
-	length := len(s)
-	for i, j, k, l := 0, 1, 2, 3; l < length; i, j, k, l = i+1, j+1, k+1, l+1 {
-		if s[i] != s[j] &&
-			s[i] != s[k] &&
-			s[i] != s[l] &&
-			s[j] != s[k] &&
-			s[j] != s[l] &&
-			s[k] != s[l] {
+	for i, j, k, l := 0, 1, 2, 3; l < len(stream); i, j, k, l = i+1, j+1, k+1, l+1 {
+		if stream[i] != stream[j] &&
+			stream[i] != stream[k] &&
+			stream[i] != stream[l] &&
+			stream[j] != stream[k] &&
+			stream[j] != stream[l] &&
+			stream[k] != stream[l] {
 			return l + 1, true
 		}
 	}
-	return -1, false
+	return 0, false
 }
